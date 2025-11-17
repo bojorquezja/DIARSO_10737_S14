50a88fec99512c2b3f92f7e9d03993aaaf48f2ad
bojorquezja <bojorquez.ja@gmail.com>
2025-11-17 18:36:51 -0500

    estado

---
+++ estado/BorradorEstadoDoc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estado {
+    internal class BorradorEstadoDoc : EstadoDoc {
+        private Documento documento;
+
+        public BorradorEstadoDoc(Documento documento) {
+            this.documento = documento;
+        }
+
+        public void Mostrar() {
+            Console.WriteLine($"Imprime {documento.Nombre} en Borrador"); 
+        }
+
+        public void Publicar() {
+            documento.CambiaEstado(new PublicadoEstadoDoc(documento));
+            Console.WriteLine($"Se publico {documento.Nombre}");
+        }
+    }
+}
---
+++ estado/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+using estado;
+
+Documento d = new Documento("El quijote");
+d.Mostrar();
+d.Publicar();
+d.Mostrar();
+d.Publicar();
+d.Mostrar();
+d.Publicar();
+
